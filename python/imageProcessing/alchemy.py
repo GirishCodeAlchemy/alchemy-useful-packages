3c0ce7d1daaad3aef6e5bcaf22ae7695185f81ae
girish-devops-project <venkatareddygirish@gmail.com>
2024-01-11 20:50:47 -0500

    Updated wordcloud with size

--- python/imageProcessing/alchemy.py
+++ python/imageProcessing/alchemy.py
@@ -1,17 +1,19 @@
 from PIL import Image
 
-img = Image.open("../assets/profile.png")
-
+filename = "../assets/profile.png"
+filename = "/Users/girish/Desktop/workspace/GirishCodeAlchemy/alchemy/alchemy-useful-packages/usefulpackages.png"
+sizes = (500, 500)
+img = Image.open(filename)
 rgb_image = img.convert("RGB")
 
 # Resize the image
-resized_image = rgb_image.resize((600, 400))
+resized_image = rgb_image.resize(sizes)
 
 resized_image.save("resized_profile.png")
 
 
 # Resize the image while maintaining the aspect ratio
-img.thumbnail((600, 400))
+img.thumbnail(sizes)
 
 # Save the resized image to a file
 img.save("thumbnail_image.png")
